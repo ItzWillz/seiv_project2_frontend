2f101a744523e419984283790749cc14616c2caa
Skyler8212 <129219374+Skyler8212@users.noreply.github.com>
2023-08-30 14:48:25 -0500

    App page design
    I tried my best

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
+
+
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--MenuBar /-->
+    <div style="background-color: maroon; width 100%; height:50px; margin:-10px; display:block;">
+      <h1 style="color:white; text-align:center; margin:0px; padding-top:10px;">View Courses</h1>
+      </div>
+      <div class="column">
+    <div class="card">
+        <Listbox v-model="selectedCity" :options="cities" filter optionLabel="name" />
+    </div>
+      </div>
+    <div class="column">
+      <h1 style="text-align: center;">Actions</h1>
+      <button>View</button>
+      <button>Edit</button>
+      <button>Add</button>
+      <button>Delete</button>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import Listbox from 'primevue/listbox';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const selectedCity = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//import vuetify from "./plugins/vuetify.js";
+// import "primeflex/primeflex.css";
+// import "primevue/resources/themes/lara-light-blue/theme.css";
+// import "primevue/resources/primevue.min.css"; /* Deprecated */
+import "primeicons/primeicons.css";
+import "./style.css";
+// import "./flags.css";
 
-createApp(App).mount('#app')
+//import { createApp } from "vue";
+//import App from "./App.vue";
+//import { router } from "./router";
+import PrimeVue from "primevue/config";
+import Listbox from 'primevue/listbox';
+
+
+
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true  });
+
+app.component('list-box', Listbox);
+
+//app.mount("#app");
+
+
+// createApp(App).mount('#app')
+app.mount("#app");
+
+// .use(router).mount("#app");
