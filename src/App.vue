359db8ee3bbaf5fd35d10af188359fdec307ef68
ItzWillz <william.x.mcdonald@eagles.oc.edu>
2023-08-31 15:03:33 -0500

    Services and Testing
    title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="card text-center m-3">
+        <div class="card-body">New course courseNum, maybe:</div>
+    </div>
+
 </template>
 
 <script>
@@ -12,6 +17,40 @@ export default {
     HelloWorld
   }
 }
+
+// Post attempt with axios
+// import axios from "axios";
+// axios.post('http://localhost:8081/', {
+//       courseNum: 'Test',
+//        dept: 'test',
+//        level: '1',
+//        hours: '1',
+//        name: 'test'
+// }).then(function (response) {
+//   console.log(response);
+// });
+
+// // POST request using fetch with set headers
+// import CourseServices from "../courseServices.js";
+// import { useRouter } from "vue-router";
+
+// const router = useRouter();
+//   const data = {
+//        courseNum: 'Test',
+//        dept: 'test',
+//        level: '1',
+//        hours: '1',
+//        name: 'test'
+//   };
+//   CourseServices.create(data)
+//     .then((response) => {
+//       console.log("add " + response.data);
+//       router.push({ name: "courses" });
+//     })
+//     .catch((e) => {
+//       console.log(e.response.data.message);
+//     });
+
 </script>
 
 <style>
