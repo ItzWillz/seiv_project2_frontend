33bba6b197e210cfded319fac2c859e5029d7649
ItzWillz <william.x.mcdonald@eagles.oc.edu>
2023-09-04 18:01:28 -0500

    Rearranged
    Rearranged some stuff that broke the Listbox on merge.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
-  <ViewCourse />
+    <!--MenuBar /-->
+    <!--ViewCourse /-->
+    <div style="background-color: maroon; width 100%; height:50px; margin:-10px; display:block;">
+      <h1 style="color:white; text-align:center; margin:0px; padding-top:10px;">View Courses</h1>
+      </div>
+
+      <div class="column">
+    <div class="card flex justify-content-center">
+        <Listbox v-model="selectedCity" :options="cities" filter optionLabel="name" />
+    </div>
+    <div style="margin-top: 7rem"> 
+      <h1 style="text-align: center;">Actions</h1>
+      <div class="row">
+      <button >View</button>
+      <button>Edit</button>
+      </div>
+      <div class="row">
+      <button>Add</button>
+      <button>Delete</button>
+      </div>
+      </div>
+    </div>
+    
+    <div class="card text-center m-3">
+        <div class="card-body">New course courseNum, maybe:</div>
+    </div>
 </template>
 
-<script>
-import ViewCourse from './components/addCourse.vue'
+<script setup>
+import { ref } from "vue";
+import Listbox from 'primevue/listbox';
+//import ViewCourse from './components/addCourse.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 
 //export default {
 //  name: 'App',
@@ -11,7 +45,6 @@ import ViewCourse from './components/addCourse.vue'
 //    ViewCourse
 //  }
 //}
-
 
 // Post attempt with axios
 // import axios from "axios";
@@ -70,18 +103,19 @@ fetch('http://localhost:8081/', requestOptions)
 //     });
 
 
-const selectedCity = ref();
-const cities = ref([
-    { name: 'Course', code: 'NY' },
-    { name: 'Course', code: 'RM' },
-    { name: 'Course', code: 'LDN' },
-    { name: 'Course', code: 'IST' },
-    { name: 'Course', code: 'PRS' },
-    { name: 'Course', code: '3' },
-    { name: 'Course', code: '4' }
-]);
+// const selectedCity = ref();
+// const cities = ref([
+//     { name: 'Course', code: 'NY' },
+//     { name: 'Course', code: 'RM' },
+//     { name: 'Course', code: 'LDN' },
+//     { name: 'Course', code: 'IST' },
+//     { name: 'Course', code: 'PRS' },
+//     { name: 'Course', code: '3' },
+//     { name: 'Course', code: '4' }
+// ]);
 
 </script>
+
 
 <style>
 #app {
