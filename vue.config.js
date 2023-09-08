10a83fa49dd7ff691d26d747e0ee02bf89b823a4
Andrew Moyer <andrew.moyer@eagles.oc.edu>
2023-09-08 09:39:07 -0500

    Trying AWS to work

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   devServer: {
     host: 'localhost',
   },
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/var/www/html/2023/project2/t2/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/2023/project2/t2/',
   transpileDependencies: ['vuetify'],
 };
