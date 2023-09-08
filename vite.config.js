6e7f5d11cbf47f407af7eacf36764a45755943fe
Andrew Moyer <andrew.moyer@eagles.oc.edu>
2023-09-07 23:34:29 -0500

    Testing with AWS

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 
 export default () => {
   const baseURL =
-    process.env.APP_ENV === "development" ? "/" : "/course-t2/";
+    process.env.APP_ENV === "development" ? "/" : "/var/www/html/2023/project2/t2/";
 
   return defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
